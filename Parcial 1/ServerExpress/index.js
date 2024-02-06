5c0c033a921766ca21c3d4c271481c3c417e6bbf
nayaracasz <nayaracs208@gmail.com>
2024-02-06 09:42:57 -0600

    add middleware

--- Parcial 1/ServerExpress/index.js
+++ Parcial 1/ServerExpress/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+//middleware: funcion que se ejecuta en el ciclo de solicitud de respuesta
+
+//middleware creado de 0
+app.use((req, res, next)=>{
+    console.log("Peticion al server " + new Date());
+    next();
+})
+
+//middleware instalado de un npm
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
     res.send('Contestando a get desde Server Express');
